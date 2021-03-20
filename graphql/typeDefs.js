cc9a469f9cb455fb8b02ac3d96dfd8bc71e8bd96
Juni <yunior.varon@yahoo.com>
2021-03-20 12:55:36 +0100

    create the message query and mutation

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -9,21 +9,28 @@ module.exports = gql`
     createdAt: String!
   }
 
+  type Message {
+    uuid: String!
+    content: String!
+    from: String!
+    to: String!
+    createdAt: String!
+  }
+
   type Query{
     getUsers:[User]!
     login(username:String! password: String!):User!
+    getMessages(from:String!):[Message]!
   }
 
   # All Mutation
   type Mutation{
-    register(input: registerInput):User!
-  }
-
-  # All custom input
-   input registerInput{
-    username: String!
+    register(username: String!
     password: String!
     confirmPassword: String!
-    email: String!
+    email: String!):User!
+
+    # Message mutation
+    sendMessage(to:String! content:String!): Message!
   }
 `
